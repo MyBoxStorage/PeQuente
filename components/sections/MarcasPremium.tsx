b8d05d7f8e0b822166367db64bd771fd52ef5bad
MyBoxStorage <techgeniushq@gmail.com>
2026-01-04 15:48:28 -0300

    feat: alternar cores vermelho e azul nos cards de Marcas Premium

--- components/sections/MarcasPremium.tsx
+++ components/sections/MarcasPremium.tsx
@@ -22,23 +22,31 @@ export default function MarcasPremium() {
           <div
             className="flex gap-8 overflow-x-auto scroll-smooth pb-4 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
           >
-            {brands.map((brand) => (
-              <Link
-                key={brand.id}
-                href={`/produtos?marca=${brand.name.toLowerCase()}`}
-                className="flex-shrink-0 group"
-              >
-                <div className="bg-[#1a1a1a] rounded-lg p-8 border border-[#252525] hover:border-[#FF0000] transition-all duration-300 w-48 h-48 flex items-center justify-center hover:bg-[#252525]">
-                  <Image
-                    src={`/images/brands/${brand.slug}.png`}
-                    alt={brand.name}
-                    width={128}
-                    height={128}
-                    className="object-contain opacity-70 group-hover:opacity-100 transition max-w-[128px] max-h-[128px]"
-                  />
-                </div>
-              </Link>
-            ))}
+            {brands.map((brand, index) => {
+              const isRed = index % 2 === 0;
+              const bgColor = isRed ? 'bg-[#FF0000]' : 'bg-[#00008B]';
+              const hoverBgColor = isRed ? 'hover:bg-[#CC0000]' : 'hover:bg-[#000066]';
+              const borderColor = isRed ? 'border-[#FF0000]' : 'border-[#00008B]';
+              const hoverBorderColor = 'hover:border-white';
+              
+              return (
+                <Link
+                  key={brand.id}
+                  href={`/produtos?marca=${brand.name.toLowerCase()}`}
+                  className="flex-shrink-0 group"
+                >
+                  <div className={`${bgColor} ${hoverBgColor} rounded-lg p-8 ${borderColor} ${hoverBorderColor} transition-all duration-300 w-48 h-48 flex items-center justify-center`}>
+                    <Image
+                      src={`/images/brands/${brand.slug}.png`}
+                      alt={brand.name}
+                      width={128}
+                      height={128}
+                      className="object-contain opacity-90 group-hover:opacity-100 transition max-w-[128px] max-h-[128px] brightness-0 invert"
+                    />
+                  </div>
+                </Link>
+              );
+            })}
           </div>
 
         </div>
